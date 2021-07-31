db52be9b6b073f818befd134a37bcdd29ff1d7eb
dhandyjoe <dhanjoenkp@gmail.com>
2021-08-01 01:58:21 +0700

    update materi

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts/main')
+
+@section('container')
+
+    <h1>Halaman Home</h1>
+    <h3>{{ $name }}</h3>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 	return view('home', [
+		"title" => "Home",
 		"name" => "Dhandy Joenathan"
 	]);
 });
 
 Route::get('/about', function () {
-	return view('about');
+	return view('about', [
+		"title" => "About"
+	]);
 });
 
 Route::get('/profile', function () {
-	return view('profile');
+	return view('profile', [
+		"title" => "Profile",
+	]);
 });
